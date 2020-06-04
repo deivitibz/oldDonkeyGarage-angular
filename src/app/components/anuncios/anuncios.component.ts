1b28cc4a23ac37fd0a7893e1d6f0daf375144a3f
David Perez <rpprec@gmail.com>
2020-06-04 16:03:27 +0200

    maquetacion barra de navegacion y formularios

--- src/app/components/anuncios/anuncios.component.ts
+++ src/app/components/anuncios/anuncios.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import * as brands from '../../db/moto_brands.json';
+import * as models from '../../db/moto_models.json';
+
 @Component({
   selector: 'app-anuncios',
   templateUrl: './anuncios.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class anunciosComponent implements OnInit {
 
-  constructor() { }
+  marcas: any;
+  modelos: any;
+
+  form: FormGroup;
+
+  constructor() {
+    this.marcas = brands.data;
+    this.modelos = models.data;
+    this.form = new FormGroup({
+      titulo: new FormControl('',[]),
+      descripcion: new FormControl ('',[]),
+      precio: new FormControl ('',[]),
+      marca: new FormControl ('',[]),
+      modelo: new FormControl ('',[]),
+      tipoCustom: new FormControl ('',[])
+
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  filtrarMarcas(form){
+    console.log(form.value);
+
+  }
+
+  onSubmitFormulario(forms){
+    console.log(forms.value);
+
   }
 
 }
