b028755ca38ad347d9672ab1e3c2ce79ec0aa609
Raymon Bye <molinero.gomez.ramon@gmail.com>
2020-06-12 09:05:55 +0200

    servicios noticias

--- src/app/servicios/noticia.service.ts
+++ src/app/servicios/noticia.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Noticia } from '../models/noticia.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoticiaService {
+  baseUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'http://streaming.zapto.org:3000/api/noticia';
+  }
+
+  getNoticia(id) {
+    return this.httpClient.get(this.baseUrl + '/' + id).toPromise();
+  }
+
+  getAllNoticias() {
+    return this.httpClient.get(this.baseUrl).toPromise();
+  }
+
+  newNoticia(noticia: Noticia) {
+    return this.httpClient.post(this.baseUrl, noticia).toPromise();
+  }
+
+  deleteNoticia(id) {
+    return this.httpClient.delete(this.baseUrl, id).toPromise();
+  }
+
+  async editNoticia(id) {
+    const noticia = await this.getNoticia(id);
+    return this.httpClient.put(this.baseUrl, noticia).toPromise();
+  }
 }
